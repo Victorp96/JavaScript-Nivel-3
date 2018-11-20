69fb202a027af00b9a0e1261b6b67adc180497d7
cadif1 <cadif1@hotmail.com>
2018-11-20 12:24:34 -0400

    commit 2 clase 2

--- Clase 2/clase2_2.js
+++ Clase 2/clase2_2.js
@@ -15,7 +15,20 @@ $("#subtitulo").click(function(){
 $("div h2").click(function(){
     console.log("hola Victor")
 })
+$("#esconder").click( function (){
+    //let x = $("ol");
+    if (this.value == "Esconder"){
+        $("ol").hide();
+        $("#esconder").val("Mostrar")
+    }else{
+        $("ol").show();
+        $("#esconder").val("Esconder")
+    }
+})
 $("#agregar").click(function(){
     let dato = prompt("Introduzca un texto:");
     $("ol").append("<li>"+dato+"</li>");
+})
+$("h1").click(function(){
+    $("h1").height($("h1").height()+10)
 })
